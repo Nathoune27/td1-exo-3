5dab2e3c0cad406d1aeeb87a0952169b6d53deb0
Nathoune27 <68045427+Nathoune27@users.noreply.github.com>
2020-07-09 16:43:11 +1100

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    if (nb < 10) {
+        nb += 1
+        basic.showNumber(nb)
+    }
+    basic.showNumber(nb)
+})
+let nb = 0
+nb = 1
+basic.showNumber(nb)
